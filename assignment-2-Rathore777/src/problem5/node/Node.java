80ff5794150d70531ac6d04ebaba01a71913dde6
Utkarsh Rathore <utkarsh.rathor_cs18@gla.ac.in>
2020-03-27 23:04:46 +0530

    Add files via upload

---
+++ assignment-2-Rathore777/src/problem5/node/Node.java
@@ -0,0 +1,39 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 23-Mar-20
+ *  Time: 7:06 PM
+ */
+package problem5.node;
+// to define node properties
+public class Node {
+    private Student s;
+    private  Node next;
+
+
+    public Student getS() {
+        return s;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+
+    public void setS(Student s) {
+        this.s = s;
+    }
+
+    public Node getNext()
+    {
+        return next;
+    }
+
+
+
+    public Node(Student s)
+    {
+        this.s = s;
+        next=null;
+    }
+}
